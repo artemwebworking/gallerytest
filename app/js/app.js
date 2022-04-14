f82d7ccdb8fe1d1c22924e6da13b654396381185
artemwebworking <artemwebworking@gmail.com>
2022-04-14 09:22:09 +0300

    gallery done

--- app/js/app.js
+++ app/js/app.js
@@ -9,11 +9,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	
 	// Custom JS
 	const sliderMain = new Swiper('.slider_main', {
-		freeMode: true,
-		centeredSlides: true,
-		mousewheel: true,
-		parallax: true,
-		breakpoints: {
+	freeMode: true,
+	centeredSlides: true,
+	mousewheel: true,
+	parallax: true,
+	breakpoints: {
 			0: {
 				slidesPerView: 2.5,
 				spaceBetween: 20
@@ -31,15 +31,16 @@ document.addEventListener('DOMContentLoaded', () => {
 		slidesPerView: 3.5
 	})
 	sliderMain.controller.control = sliderBg
-
+	
 	document.querySelectorAll('.slider__item').forEach(item => {
 		item.addEventListener('click', event => {
 			item.classList.toggle('opened')
 		})
 	})
-
+	
 	let desc = document.querySelector('.description')
 	sliderMain.on('slideChange', e => {
 		sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
 	})
+
 })
